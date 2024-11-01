4cdf5dd1f21049d94f2a4a53b7033f78f8756908
galih <samudragalih94@gmail.com>
2024-11-01 09:24:43 +0700

    wip initial push

---
+++ DiceRoll/DiceRoll/DiceRollApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollApp.swift
+//  DiceRoll
+//
+//  Created by Galih Samudra on 01/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
